59536766ff8739d51945a69c7f2f27bbf8bab402
Sher Ning <hi@sherning.com>
2023-03-08 18:57:19 +0800

    deep understanding of python modules and packages

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
+import importlib
 
-# closure is a function with a free variable
+import new_module
+import types
+import socket, math
+import sys
+from pprint import pprint
 
-def outer():
-    a = 25
-    name = "python"
+# importing new_module will execute new_module line by line
+# a module is also an object in python
 
-    def inner(prefix):
-        print(prefix, name)
+print(globals()['new_module'])
+# "<module 'new_module'>"
 
-    return inner
+# proves that module is an object in memory
+print(type(new_module))
+print(isinstance(new_module, types.ModuleType))
+print(hex(id(new_module)))
+
+my_mod = new_module
+# same address in memory
+print(hex(id(my_mod)))
+
+# list of PyObjects/Attributes in New_Module
+# after importing new_module, module object attributes available
+print(dir(new_module))
+
+# __name__ -> new module
+print(new_module.__dict__['__name__'])
+print(new_module.__name__)  # made simpler
+print(new_module.__file__)  # file path
+
+new_module.__dict__['my_func']()  # you can invoke function object
 
 
-my_func = outer()
-
-# the name variable is a free variable with a reference count
-my_func("hi")
+print(globals()['math'])
+print(globals()['socket'])
 
 
-def counter(start):
+# finder and loader
+pprint(sys.meta_path)
+importer = sys.meta_path[2]
 
-    def increment(step=1):
-        nonlocal start
-        start += step
-        print(start)
-
-    return increment
+# spec = importer.find_spec('itertools')
+pprint(sys.modules)  # prints all pre-loaded module
 
 
-my_func = counter(1)
-# start is the free variable
-my_func(1)
-my_func(1)
-my_func(1)
-my_func(1)
-my_func(1)
-my_func(1)
+del socket
+print('socket' in globals())  # false
+print('socket' in sys.modules)  # true
+
+# another way of importing module instead of import
+import_me = importlib.import_module('import_me')
+print(globals()['import_me'])
+import_me.hola()
+
+# from math import ...
+# python does not do partial loading, it will load everything in that module
+
+# __main__ is the execution entry point
+
+# package is a directory or folder of modules
+# module is a single file
+
+# package has path
+import collections
+print(collections.__path__)
+# print(socket.__path__) -> undefined
+
+# regular packages
+# __init__.py -> package
+# will be executed automatically like a constructor call()
+print(collections.__file__)
+
+# print the __init__.py path
+
+# importing package does not import modules inside
+# import collections.mod1 -> import module inside package
+
+# absolute import means pack1.pack2.mod2 -> outer to inner
+
+# from . import mod2 -> current directory
+# from .. import mod2 -> parent directory
+# can only be used inside packages -> contain __init__
+
+# _x prefix with _ will not be exported
+
+# __all__ ["y", "method name"] -> will be exported
 
---
+++ new_module.py
@@ -0,0 +1,13 @@
+
+print("starting module")
+
+x = 24
+
+
+def say_hello():
+    print(f"{__name__} says hello")
+
+
+def my_func():
+    # python create these dunder files and can be used a global variables
+    print(__file__)
